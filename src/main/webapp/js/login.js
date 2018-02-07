0f99111f9073b98f1b38644595c5be527b635db8
namngo <hnnngo@mum.edu>
2018-02-06 20:05:00 -0600

    Add user form

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,6 +1,18 @@
 $(function() {
+    function showAddUserView() {
+        UserDetail.buildAddUserView("popup", addCallback);
+        $("#popup").dialog({ modal: true, minWidth: 750, maxWidth: 750, minHeight: 600, title: "Add New User" });
+    }
+
+    function addCallback(data) {
+        $("#popup").dialog("close");
+    }
+
     window.onload = function() {
         CarStoreCommon.resetHeaderButtons().done(function() {
+            $("#create-user-action").off('click').on('click', function() {
+                showAddUserView();
+            });
         });
     }
 });
